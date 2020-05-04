ea75072d063cc6c657533d087678de6f8659f8d2
Rana-Siroosian <rana.siroosian@gmail.com>
2020-05-04 16:25:08 -0400

    still learning JavaFX

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,27 @@
 package sample;
-
+/**
+ * 
+ * @author >>RanaSiroosian<<
+ */
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class Controller {
     @FXML private Text actiontarget;
+    @FXML private TextField textField;
+    @FXML private PasswordField passwordField;
+    
     
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
         actiontarget.setText("Sign in button pressed");
+    }
+    
+    @FXML protected void handleClearButtonAction(ActionEvent event) {
+    	textField.clear();
+    	passwordField.clear();
+    	actiontarget.setText("Please fill the required fields again!");
     }
 }
